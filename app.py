d92031dfbb63fb1c467e37066c2c437d1bfb0fed
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-12-27 21:57:14 +0530

    Data Ingestion Pipeline Complete.

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from src.logging import logger
+import sys
+from src.exception.exception import NetworkSecurityException
+from src.logging.logger import logging
+from src.components.data_ingestion import DataIngestion
+from src.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 
-logger.logging.info(" app Logging Setup Complete")
+if __name__ == "__main__":
+    try:
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        artifact = data_ingestion.initiate_data_ingestion()
+        print(artifact)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
